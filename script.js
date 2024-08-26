467de75c593288ee3014c5791803a50c5e7dac26
saeed rastegar <saeed.rastegarrazaghi@tu-clausthal.de>
2024-08-26 23:10:33 +0200

    Game logic successfully implemented

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 'use strict';
 
-// get a given number
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct selectedNumber';
+// document.querySelector('.selectedNumber').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// console.log(document.querySelector('.guess').value);
 
-let randomNumber = Math.floor(Math.random() * 20) + 1;
+const selectedNumber = Math.trunc(Math.random() * 20) + 1;
 
-function getValue() {
-  let input = document.getElementsByClassName('guess')[0];
-  let value = input.value;
-  console.log('Your number is: ' + value);
-  console.log('The exact number is: ' + randomNumber);
+let score = 20;
 
-  if (value > randomNumber) {
-    console.log('too High');
-  } else if (value < randomNumber) {
-    console.log('too low');
-  } else {
-    console.log('Good guess');
+document.querySelector('.number').textContent = selectedNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No Number!';
+  } else if (guess === selectedNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > selectedNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤦‍♂️ You lost the game!';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  } else if (guess < selectedNumber) {
+    if (score > 0) {
+      // Changed 'else' to 'else if' with condition
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--; // Corrected message
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🤦‍♂️ You lost the game!';
+    }
   }
-}
+});
