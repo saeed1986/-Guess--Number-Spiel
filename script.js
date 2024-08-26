d261e4cc1e7fe540ce88f338e87a0eb9ecca398e
saeed rastegar <saeed.rastegarrazaghi@tu-clausthal.de>
2024-08-26 23:36:06 +0200

    Address partial functionality issue with the again button

--- script.js
+++ script.js
@@ -6,20 +6,26 @@
 // document.querySelector('.score').textContent = 10;
 // console.log(document.querySelector('.guess').value);
 
-const selectedNumber = Math.trunc(Math.random() * 20) + 1;
+const selectedNumber = Math.trunc(Math.random() * 100) + 1;
 
 let score = 20;
-
-document.querySelector('.number').textContent = selectedNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫 No Number!';
+
+    // When player wins
   } else if (guess === selectedNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = selectedNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When quess is too high
   } else if (guess > selectedNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -30,6 +36,8 @@ document.querySelector('.check').addEventListener('click', function () {
       score = 0;
       document.querySelector('.score').textContent = score;
     }
+
+    // When quess is too low
   } else if (guess < selectedNumber) {
     if (score > 0) {
       // Changed 'else' to 'else if' with condition
@@ -40,4 +48,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🤦‍♂️ You lost the game!';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing ...';
 });
